4a39e1c474290152fcfd7a3026c05463b9d4ae3e
Rickey Zachary <rzachary@Rickeys-MacBook-Pro.local>
2020-04-13 16:10:28 -0400

    Initial Push of Action for S3 Bucket Pushes

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8-alpine
+
+LABEL "com.github.ations.name"="S3 Blog Push"
+LABEL "com.github.actions.description"="Push blog contents to an S3 Bucket"
+LABEL "com.github.actions.icon"="refresh-ccw"
+LABEL "com.github.action.color"="yellow"
+
+LABEL version="0.0.1"
+LABEL repository=""
+LABEL homepage="rickeyzachary.com"
+LABEL maintainer="Rickey Zachary <rzachary@rickeyzachary.com>"
+
+ENV AWSCLI_VERSION='1.18.39'
+
+RUN pip install --quiet --no-cache-dir awscli==${AWSCLI_VERSION}
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+# check to see the AWS_REGION is set if not set it to the Default us-east-1
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+# check to see if the S3_BUCKET Environment variable is set, quit if it is not set
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+# check to see if the AWS ACCESS KEY is set, quit if it is not set
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+# check to see if the AWS ACCESS SECRET KEY is set
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
+if [ -n "$AWS_S3_ENDPOINT" ]; then
+  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
+fi
+
+# create a temporary aws profile to run the command 
+aws configure --profile s3-bucket-push <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Sync using our dedicated profile and suppress verbose messages.
+# All other flags are optional via the `args:` directive.
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile s3-sync-action \
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
+
+
+aws configure --profile s3-bucket-push <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
